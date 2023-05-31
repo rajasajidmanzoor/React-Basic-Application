fecc79d9bcde96a91a1344025fc249b87eea7e52
Sajid Manzoor <sajidraja1@yahoo.com>
2023-05-31 17:40:07 +0500

    Basic Routes setup for Backend User Authentication

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ React Basic Application With Express, Node, Mongo DB, redux toolkit. Application
 ## Clone 
 git clone https://github.com/rajasajidmanzoor/React-Basic-Application.git
 
+## Dependencies
+This project uses:
+- express
+- dotenv 
+- mongoose 
+- bcryptjs 
+- jsonwebtoken 
+- cookie-parser
+- express-async-handler
---
+++ backend/controllers/userController.js
@@ -0,0 +1,50 @@
+import asyncHandler from "express-async-handler";
+
+
+//@desc Auth user/set token
+// route POST /api/users/auth
+// @access Public
+
+const authUser = asyncHandler(async (req, res)=>{
+    res.status(200).json({ message: 'Auth User'});
+});
+
+//@desc Register a  new user
+// route POST /api/users/
+// @access Public
+
+const registerUser = asyncHandler(async (req, res)=>{
+    res.status(200).json({ message: 'Register User'});
+});
+
+//@desc Logout a User
+// route POST /api/users/logout
+// @access Public
+
+const logoutUser = asyncHandler(async (req, res)=>{
+    res.status(200).json({ message: 'Logout User'});
+});
+
+//@desc Get a User Profile
+// route GET /api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res)=>{
+    res.status(200).json({ message: 'User Profile'});
+});
+
+//@desc Update a User Profile
+// route PUT  /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res)=>{
+    res.status(200).json({ message: 'Update User Profile'});
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+}
