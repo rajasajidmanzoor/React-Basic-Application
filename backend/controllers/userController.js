23560edfb0295beed504c872ba332a657052c6b2
Sajid Manzoor <sajidraja1@yahoo.com>
2023-06-01 01:10:04 +0500

    Register, Login and Logout Endpoints.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,26 @@
 import asyncHandler from "express-async-handler";
-
+import generateToken from "../utils/generateToken.js";
+import User from "../models/userModel.js";
 
 //@desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 
 const authUser = asyncHandler(async (req, res)=>{
+    const {email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(user && (await user.matchPassword(password))){
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    }
+    else {
+        res.status(401);
+        throw new Error('User does not Exists!');
+    }
     res.status(200).json({ message: 'Auth User'});
 });
 
@@ -14,7 +29,31 @@ const authUser = asyncHandler(async (req, res)=>{
 // @access Public
 
 const registerUser = asyncHandler(async (req, res)=>{
-    res.status(200).json({ message: 'Register User'});
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({email: email});
+    if(userExists) {
+        res.status(400);
+        throw new Error('User already Exists');
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+    if(user){
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+    // res.status(200).json({ message: 'Register User'});
 });
 
 //@desc Logout a User
@@ -22,7 +61,11 @@ const registerUser = asyncHandler(async (req, res)=>{
 // @access Public
 
 const logoutUser = asyncHandler(async (req, res)=>{
-    res.status(200).json({ message: 'Logout User'});
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({ message: 'User Logged out'});
 });
 
 //@desc Get a User Profile
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
-import { notFound, errorHandler
- } from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDb from './config/db.js';
 connectDb();
-
- import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/api/users', userRoutes);
 app.get('/', (req, res)=> res.send('Server is Ready'));
 
